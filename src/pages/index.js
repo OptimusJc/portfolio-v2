eee9a8d3e8f147207e4bf2ab9422e9ce9e183d1e
OptimusJc <optimus.prime.jc@gmail.com>
2022-09-07 20:10:20 +0300

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <section>
+      <h1>Welcome to my portfolio site!</h1>
+    </section>
+  );
+}
+export default IndexPage
+
+export const Head = () => <title>Joseph's Portfolio</title>
