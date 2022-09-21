f63ec64ca4b6f7c343094f528dbde3cd50fde1ef
OptimusJc <optimus.prime.jc@gmail.com>
2022-09-21 17:26:23 +0300

    chore: Troubleshooting why it's not building

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,23 @@
 module.exports = {
-  siteMetadata: {
-    title: `portfolio-v2`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+	flags: {
+		DEV_SSR: true,
+	},
+	siteMetadata: {
+		title: `portfolio-v2`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		"gatsby-plugin-sass",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+	],
 };
